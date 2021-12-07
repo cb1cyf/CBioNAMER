453302b102b4f2d32486c47993c0baccf8426830
cb1cyf <41017545+cb1cyf@users.noreply.github.com>
2021-12-07 20:47:08 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="nner",                                     # 包的分发名称，使用字母、数字、_、-
-    version="0.1.1",                                        # 版本号, 版本号规范：https://www.python.org/dev/peps/pep-0440/
+    name="CBioNAMER",                                     # 包的分发名称，使用字母、数字、_、-
+    version="1.0",                                        # 版本号, 版本号规范：https://www.python.org/dev/peps/pep-0440/
     author="Yufeng Cheng",                                       # 作者名字
     author_email="2101210556@pku.edu.cn",                      # 作者邮箱
-    description="Nested Named Entity Recognition",                            # 包的简介描述
+    description="Nested Named Entity Recognition for Chinese Biomedical Text",                            # 包的简介描述
     long_description=long_description,                      # 包的详细介绍(一般通过加载README.md)
     long_description_content_type="text/markdown",          # 和上条命令配合使用，声明加载的是markdown文件
-    url="https://github.com/cb1cyf/NNER/",                              # 项目开源地址，我这里写的是同性交友官网，大家可以写自己真实的开源网址
+    url="https://github.com/cb1cyf/CBioNAMER/",                              # 项目开源地址，我这里写的是同性交友官网，大家可以写自己真实的开源网址
     packages=setuptools.find_packages(),                    # 如果项目由多个文件组成，我们可以使用find_packages()自动发现所有包和子包，而不是手动列出每个包，在这种情况下，包列表将是example_pkg
     classifiers=[                                           # 关于包的其他元数据(metadata)
         "Programming Language :: Python :: 3",              # 该软件包仅与Python3兼容
